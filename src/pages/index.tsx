bf4830f724e15ef626bf70d163201bf2faeb197b
Tarcizio Barbosa - Dislog <tarcizio.barbosa@outlook.com>
2022-01-07 17:17:30 -0300

    improve index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,28 @@
 import Iframe from "react-iframe";
 import type { NextPage } from "next";
-import { useContext, useEffect } from "react";
+import { FormEvent, MouseEventHandler, useContext, useEffect } from "react";
 import { AuthContext } from "../contexts/auth";
 import { useRouter } from "next/router";
+import { Disclosure, Menu, Transition } from "@headlessui/react";
+import profilePic from "../public/user.png";
+import { BellIcon } from "@heroicons/react/solid";
+import Image from "next/image";
+import { Fragment } from "react";
+
+const navigation = [
+  { name: "Qualidade do Pedido", href: "#", current: true },
+  { name: "Cockpit", href: "#", current: false },
+  { name: "Outros", href: "#", current: false },
+];
+
+const userNavigation = [{ name: "Sign Out", href: "#" }];
+
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(" ");
+}
 
 const Home: NextPage = () => {
-  const { user } = useContext(AuthContext);
+  const { user, signOut } = useContext(AuthContext);
   const router = useRouter();
 
   useEffect(() => {
@@ -17,6 +34,98 @@ const Home: NextPage = () => {
   return (
     <>
       <div className="min-h-full">
+        <Disclosure as={"nav"} className={"bg-gray-800"}>
+          {({ open }) => (
+            <>
+              <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                <div className="flex items-center justify-between h-16">
+                  <div className="flex items-center">
+                    <div className="flex-shrink-0">
+                      <img
+                        className="h-8 w-8"
+                        src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
+                        alt="Workflow"
+                      />
+                    </div>
+                    <div className="hidden md:block">
+                      <div className="ml-10 flex items-baseline space-x-4">
+                        {navigation.map((item) => (
+                          <a
+                            key={item.name}
+                            href={item.href}
+                            className={classNames(
+                              item.current
+                                ? "bg-gray-900 text-white"
+                                : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                              "px-3 py-2 rounded-md text-sm font-medium"
+                            )}
+                            aria-current={item.current ? "page" : undefined}
+                          >
+                            {item.name}
+                          </a>
+                        ))}
+                      </div>
+                    </div>
+                  </div>
+                  <div className="hidden md:block">
+                    <div className="ml-4 flex items-center md:ml-6">
+                      <button
+                        type="button"
+                        className="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+                      >
+                        <span className="sr-only">View</span>
+                        <BellIcon className="h-6 w-6" aria-hidden="true" />
+                      </button>
+
+                      <Menu as={"div"} className="ml-3 relative">
+                        <div>
+                          <Menu.Button className="max-w-xs bg-gray-800 rounded-full flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+                            <span className="sr-only">Open user menu</span>
+                            <Image
+                              src={profilePic}
+                              alt="User Avatar"
+                              height={28}
+                              width={28}
+                            />
+                          </Menu.Button>
+                        </div>
+                        <Transition
+                          as={Fragment}
+                          enter="transition ease-out duration-100"
+                          enterFrom="transform opacity-0 scale-95"
+                          enterTo="transform opacity-100 scale-100"
+                          leave="transition ease-in duration-75"
+                          leaveFrom="transform opacity-100 scale-100"
+                          leaveTo="transform opacity-0 scale-95"
+                        >
+                          <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                            {userNavigation.map((item) => (
+                              <Menu.Item key={item.name}>
+                                {({ active }) => (
+                                  <a
+                                    onClick={signOut}
+                                    href={item.href}
+                                    className={classNames(
+                                      active ? "bg-gray-100" : "",
+                                      "block px-4 py-2 text-sm text-gray-700"
+                                    )}
+                                  >
+                                    {item.name}
+                                  </a>
+                                )}
+                              </Menu.Item>
+                            ))}
+                          </Menu.Items>
+                        </Transition>
+                      </Menu>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </>
+          )}
+        </Disclosure>
+
         <header className="bg-white shadow">
           <div className="max-w-7xl mx-auto py-6 sm:px-6 pg:px-8">
             <h1 className="text-3xl font-bold text-gray-900">Dashboard</h1>
